7880a1f203b8a261b35c851436b56a800f24578e
Christian santos <christian@therubythree.com>
2019-07-24 13:22:40 -0700

    code added to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
     end
 
     def create
-        @post = Post.new()
+        @post = Post.new(post_params)
 
         if @post.save
             redirect_to @post
@@ -19,11 +19,9 @@ class PostsController < ApplicationController
     end
 
     def show
-        @post = Post.find(params[:id])
     end
 
     def update 
-        @post = Post.find(params[:id])
         
         if @post.update(post_params)
             redirect_to @post 
@@ -34,12 +32,21 @@ class PostsController < ApplicationController
     def edit 
         @post = Post.find(params[:id])
     end 
-end
+
+    def destroy 
+        @post = Post.find(params[:id])
+        @post.destroy 
+
+        redirect_to posts_path
+    end 
+
 
     private 
 
     def post_params 
       params.require(:post).permit(:title, :content)
     end 
+
+end
 
 end
