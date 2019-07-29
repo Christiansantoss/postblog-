c050edf74deb60387c8680edcd8f25cf01c3a6ca
Christian santos <christian@therubythree.com>
2019-07-29 12:33:24 -0700

    made the blog application work by fixing syntax errors in the posts controller and in the comments controller files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
     before_action :find_post, only: [:show, :update, :edit, :destroy]
 
     def index 
-        @posts = Post.all.order("created_at DESC")
+        @posts = Post.all.order("created_at ASC")
     end 
 
     def new
@@ -28,6 +28,7 @@ class PostsController < ApplicationController
         else 
             render 'edit'
         end 
+    end
     
     def edit 
     end 
@@ -39,14 +40,13 @@ class PostsController < ApplicationController
     end 
 
 
-    private 
+private 
 
-    def post_params 
-      params.require(:post).permit(:title, :content)
-    end 
+def post_params 
+    params.require(:post).permit(:title, :content)
+end 
 
-    def find_post
-        @post = Post.find(params[:id])
-    end
+def find_post
+    @post = Post.find(params[:id])
 end
 end
